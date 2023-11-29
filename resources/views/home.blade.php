9bcf2e248204a74bae079d4c050adbaba2907091
Michael-Nobrega <2044720@student.uma.pt>
2023-11-29 18:29:18 +0000

    etc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -107,7 +107,7 @@
   <div class="car-container">
       <h3 class="car-container-header">Brand: {{ $car["brand"] }} - Model: {{ $car["model"] }} - Year: {{ $car["year"] }}</h3>
       <img class="car-container-image" src="{{ asset('/img/' . $car["image"]) }}" alt="{{ $car["brand"] }} Image" style="max-width: 75%;"> 
-      <p class="car-container-right-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit numquam quisquam quos voluptatem, culpa veniam sequi odio ex nam eius molestiae quaerat inventore delectus, dolorem quae iste, mollitia vitae cumque?</p>
+      <p class="car-container-right-text">{{$car["description"]}}</p>
   </div>
 </div>
 
--- resources/views/view-time.blade.php
+++ resources/views/view-time.blade.php
@@ -20,5 +20,42 @@
             <h5>{{$time["lap_time"]}}</h5>
         </div>
     </div>
+    <br>
+    <div class="time-container">
+        <h4 class="time-container-header">{{$time->user->name}}</h4>
+        <img class="time-container-left-image" style="width: 100%" src="{{ asset('/img/' . $time->car["image"]) }}">
+        <h6 class="car-container-right-text">{{$time->car->brand}} {{$time->car->model}} ({{$time->car->year}})</h6>
+        <h5 class="car-container-text">{{$time["lap_time"]}}</h5>
+
+    </div>
 </body>
 @endsection
+
+<style>
+.time-container{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    background-color: rgba(0, 117, 196, 0.90);
+    padding:10px;
+    margin: 10px auto;
+    border: 2px solid black;
+    box-shadow: 15px black;
+    max-width: 75%;
+}
+.time-container-header { 
+    grid-area: 1 / 1 / 2 / 4;
+    text-align: center;
+    text-justify: center;
+}
+.time-container-left-image { 
+    grid-area: 2 / 1 / 3 / 2;
+}
+.car-container-right-text { 
+    grid-area: 2 / 2 / 3 / 4;
+}
+.car-container-text { 
+    grid-area: 3 / 1 / 4 / 4; 
+}
+
+</style>
